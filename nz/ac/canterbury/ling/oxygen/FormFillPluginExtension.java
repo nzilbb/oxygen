244d145e471948d3b1d2eae7ff71eae588f26ff1
Robert Fromont <robert@fromont.net.nz>
2016-01-26 18:13:41 -0300

    Add copyright/licence to source files.

--- nz/ac/canterbury/ling/oxygen/FormFillPluginExtension.java
+++ nz/ac/canterbury/ling/oxygen/FormFillPluginExtension.java
@@ -1,3 +1,25 @@
+//
+// Copyright 2015-2016 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of nzilbb/oxygen.
+//
+//    nzilbb/oxygen is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    nzilbb/oxygen is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with nzilbb/oxygen; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
 package nz.ac.canterbury.ling.oxygen;
 
 import ro.sync.exml.plugin.document.*;
@@ -14,8 +36,8 @@ public class FormFillPluginExtension implements DocumentPluginExtension
    /**
     * Scan document looking for form fields to present to the user, so they can add the values to the document.
     *
-    * @param  context  Selection context.
-    * @return          Selection wrapped in an &lt;orig&gt; with &lt;reg&gt; alternative, wrappted in a &lt;choice&gt; tag.
+    * @param  context  Document context.
+    * @return          Document with form field markers substituted for the values entered by the user.
     */
    public DocumentPluginResult process(DocumentPluginContext context) 
    {
